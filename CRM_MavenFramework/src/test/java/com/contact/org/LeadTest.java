0ccbf166dae908439266e819de9a3cf075c2bf8c
hp <hp@LAPTOP-9MD3T8QU>
2021-10-19 13:20:39 +0530

    COMMIT NEW

---
+++ CRM_MavenFramework/src/test/java/com/contact/org/LeadTest.java
@@ -0,0 +1,18 @@
+package com.contact.org;
+
+import org.testng.annotations.Test;
+
+public class LeadTest{
+@Test
+
+	public void CreateLeadTest() {
+	System.out.println("====lead created======");
+	}
+
+
+@Test
+
+public void CreateOrg1Test() {
+System.out.println("====lead created 1======");
+}
+}
---
+++ CRM_MavenFramework/src/test/java/com/crm/org/OrgTest1.java
@@ -0,0 +1,18 @@
+package com.crm.org;
+
+import org.testng.annotations.Test;
+
+public class OrgTest1{
+@Test
+
+	public void CreateOrgTest() {
+	System.out.println("====org created======");
+	}
+
+
+@Test
+
+public void CreateOrg1Test() {
+System.out.println("====org created 1======");
+}
+}
